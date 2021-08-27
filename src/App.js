8714975eeda1cfdad45b83963d4088af49e87b15
Вікторія Юрченко <viktoriau26@gmail.com>
2021-08-28 01:08:57 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import user from './user.json';
-// import statisticalData from './statistical-data';
+import statisticalData from './statistical-data';
 import { Profile } from './components/Profile';
 import { ProfileStatsList } from './components/ProfileStatsList';
-// import { statisticalData } from './components/statisticalData';
+import { Statistics } from './components/Statistics';
 
 function App() {
   return (
@@ -21,12 +21,18 @@ function App() {
         views={user.stats.views}
         likes={user.stats.likes}
       />
+      <section className="statistics">
+        <h2 className="title">Upload stats</h2>
+        {statisticalData.map(data => (
+          <Statistics
+            id={data.id}
+            label={data.label}
+            percentage={data.percentage}
+          />
+        ))}
+      </section>
     </div>
-    // <statisticalData
-    //   id={statisticalData.id}
-    //   label={statisticalData.label}
-    //   percentage={statisticalData.percentage}
-    // />
+    // 
   )
 }
 
